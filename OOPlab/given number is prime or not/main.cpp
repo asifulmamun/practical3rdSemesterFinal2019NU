79f7865b345d2e63c071b1d0dad7d8c51f40b8cd
asifulmamun <asifulmamun@gmail.com>
2019-12-15 22:25:20 +0600

    first commit

---
+++ OOPlab/given number is prime or not/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int isPrime = 1;
+
+    int num,i;
+    cout << "Enter Any Number for check prime or not: ";
+    cin >> num;
+
+    if(num == 0 ){
+        cout << "0 is not prime number.";
+    }
+
+    else if(num == 1){
+        cout << "1 is not prime number.";
+    }
+
+
+    for(i=2; i<num; i++){
+        if(num%i == 0){
+            isPrime = 0;
+        }
+    }
+
+
+    if(isPrime > 0){
+        cout << num << " is prime number.";
+    }
+    else{
+        cout << num << " is not prime number.";
+    }
+
+    return 0;
+}
